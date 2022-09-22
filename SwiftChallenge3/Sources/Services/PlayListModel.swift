e433f90a6cb50285f895c6cf850194bc350481be
Andrey Kuznetsov <8150309@gmail.com>
2022-09-22 13:35:08 +0300

    Add Play List Model

---
+++ SwiftChallenge3/Sources/Services/PlayListModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayListModel.swift
+//  SwiftChallenge3
+//
+//  Created by Андрей Кузнецов on 22.09.2022.
+//
+
+import Foundation
+
+struct PlayListModel {
+    var playListName: String
+    var tracks: [TrackModel] = []
+}
