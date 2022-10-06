9a72121039339c643c3e647873bfcec6a68a8a53
NikitaRekaev <nvvvvk@gmail.com>
2022-10-06 13:30:58 +0600

    Added BaseView

---
+++ SwiftChallenge3/Sources/Supporting/Base/BaseView.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setup() {
+        // for ovveride
+    }
+}
---
+++ SwiftChallenge3/Sources/Supporting/Base/BaseViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BaseViewController.swift
+//  SwiftChallenge3
+//
+//  Created by Nikita Rekaev on 06.10.2022.
+//
+
+import Foundation
