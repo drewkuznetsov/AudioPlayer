551df9659c39c8618d5be2554002c6065a0d953f
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2022-10-13 13:08:41 +0300

    re-idnent

--- SwiftChallenge3/Sources/Moduls/PlayerScreen/PlayerViewController.swift
+++ SwiftChallenge3/Sources/Moduls/PlayerScreen/PlayerViewController.swift
@@ -30,28 +30,28 @@ class PlayerViewController: BaseViewController<PlayerView>  {
 
 private extension PlayerViewController {
     
-     func setupGestureRecognizer() {
+    func setupGestureRecognizer() {
         let swipeGestureRecognizerDown = UISwipeGestureRecognizer(target: self, action: #selector(didSwipeDown(_:)))
         swipeGestureRecognizerDown.direction = .down
         view.addGestureRecognizer(swipeGestureRecognizerDown)
     }
     
-     func configure(_ track: TrackModel) {
-         if let coverURL = track.coverURL?.replacingOccurrences(of: "100x100", with: "600x600") {
+    func configure(_ track: TrackModel) {
+        if let coverURL = track.coverURL?.replacingOccurrences(of: "100x100", with: "600x600") {
             selfView.trackImageView.downloadedFrom(link: coverURL)
         }
         selfView.trackNameLabel.text = track.trackName
         selfView.authorNameLabel.text = track.artistName
     }
     
-     func playTrack(previewURL: String?) {
+    func playTrack(previewURL: String?) {
         guard let url = URL(string: previewURL ?? "") else { return }
         let playerItem = AVPlayerItem(url: url)
         selfView.player.replaceCurrentItem(with: playerItem)
         selfView.player.play()
     }
     
-     func setupTarget() {
+    func setupTarget() {
         selfView.addFavoriteButton.addTarget(self, action: #selector(favoriteButtonTapped), for: .touchUpInside)
         selfView.pauseButton.addTarget(self, action: #selector(playPauseAction), for: .touchUpInside)
         selfView.dismissButton.addTarget(self, action: #selector(dismissButtonTapped), for: .touchUpInside)
@@ -63,12 +63,12 @@ private extension PlayerViewController {
         UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
             self.selfView.trackImageView.transform = .identity
         })
-        }
-
+    }
+    
     func reduceTrackImageView() {
-            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {let scale: CGFloat = 0.8
-                self.selfView.trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale) } , completion: nil)
-        }
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {let scale: CGFloat = 0.8
+            self.selfView.trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale) } , completion: nil)
+    }
 }
 
 // MARK: - Action
@@ -76,7 +76,7 @@ private extension PlayerViewController {
 @objc
 private extension PlayerViewController {
     
-     func favoriteButtonTapped(_ sender : UIButton) {
+    func favoriteButtonTapped(_ sender : UIButton) {
         let largeConfig = UIImage.SymbolConfiguration(pointSize: 22, weight: .bold, scale: .large)
         if sender.image(for: .normal) == UIImage(systemName: "heart", withConfiguration: largeConfig) {
             sender.setImage(UIImage(systemName: "heart.fill", withConfiguration: largeConfig), for: .normal)
@@ -88,7 +88,7 @@ private extension PlayerViewController {
     func playPauseAction(_ sender: UIButton) {
         let largeConfig = UIImage.SymbolConfiguration(pointSize: 28, weight: .bold, scale: .large)
         if selfView.player.timeControlStatus == .paused {
-//            delegate?.playPauseActionDelegate()
+            delegate?.playPauseActionDelegate()
             selfView.player.play()
             enLargeTrackImageView()
             sender.setImage(UIImage(systemName: "pause.fill", withConfiguration: largeConfig), for: .normal)
@@ -99,8 +99,8 @@ private extension PlayerViewController {
         }
     }
     
-     func previousTrack() {
-         delegate?.previousTrackDelegate()
+    func previousTrack() {
+        delegate?.previousTrackDelegate()
         print("Previous track tapped")
     }
     
@@ -109,7 +109,7 @@ private extension PlayerViewController {
         print("Previous track tapped")
     }
     
-     func didSwipeDown(_ sender: UISwipeGestureRecognizer) {
+    func didSwipeDown(_ sender: UISwipeGestureRecognizer) {
         self.dismiss(animated: true)
     }
     
