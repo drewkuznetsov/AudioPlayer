7a3c9ea6fc3fea9e34b0d157d4725fba6e36fb71
NikitaRekaev <nvvvvk@gmail.com>
2022-10-18 15:54:49 +0600

    Deleted unnecessary comments

--- SwiftChallenge3/Sources/Services/TrackModel.swift
+++ SwiftChallenge3/Sources/Services/TrackModel.swift
@@ -1,10 +1,3 @@
-//
-//  TrackModel.swift
-//  SwiftChallenge3
-//
-//  Created by Андрей Кузнецов on 21.09.2022.
-//
-
 import Foundation
 
 struct TrackModel {
@@ -12,7 +5,6 @@ struct TrackModel {
     let trackName: String
     let artistName: String
     let albumName: String?
-//    let coverSmallURL: String?
     let coverURL: String?
     let previewURL: String?
 }
